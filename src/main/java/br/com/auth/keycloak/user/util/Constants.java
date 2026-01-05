64e4f8891a9f9d7b2061c81a07ec45aff7ad1be5
Carlos Monteiro <85devs@gmail.com>
2026-01-05 20:02:00 -0300

    realizar algumas correções

--- src/main/java/br/com/auth/keycloak/user/util/Constants.java
+++ src/main/java/br/com/auth/keycloak/user/util/Constants.java
@@ -5,6 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
   public static final String CONFIG_KEY_JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+  //public static final String CONFIG_KEY_JDBC_URL = "jdbc:mysql://mysql8:3306/auth-keycloak?createDatabaseIfNotExist=true&serverTimezone=GMT-3";
   public static final String CONFIG_KEY_JDBC_URL = "jdbc:mysql://192.168.100.138:3306/auth-keycloak?createDatabaseIfNotExist=true&serverTimezone=GMT-3";
   public static final String CONFIG_KEY_DB_USERNAME = "root";
   public static final String CONFIG_KEY_DB_PASSWORD = "root";
